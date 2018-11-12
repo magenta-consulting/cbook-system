c43f2e40f2b06a0b59917825f053ae0f95888b58
binhle410 <binh@sunrise.vn>
2018-11-13 01:21:41 +0700

    ok em yeu

--- libraries/bundle/MagentaCBookAdminBundle/src/Controller/BookPWAController.php
+++ libraries/bundle/MagentaCBookAdminBundle/src/Controller/BookPWAController.php
@@ -23,10 +23,21 @@ class BookPWAController extends Controller
         /**
          * {"endpoint":"https://fcm.googleapis.com/fcm/send/e1vbWsIR4ew:APA91bEyuhRU3gVr93sd-A5Kyls6RD4OhY4wxwfK-hPisTP_zSr21X33XiLHNcPOYPZDJE2tswNvksaCFDBALIdeyWp5WncOID5-QAcqUiHhoh25Xyi6phuvAjke1uJxc7Ys1S9fTsYc","expirationTime":null,"keys":{"p256dh":"BIT1e4n6h29d0NLBjLOrtBDJ69mOXGke3aq9rjRH3OG9cuihV2mvB_8_ATcaYt6pKcVjv8TR5kwfCzSNA1WeAOo","auth":"OOhi3n0xQ9LI-a5k7AsCDg"}}
          */
-        $endpoint = $request->request->get('endpoint');
-        $expirationTime = $request->request->get('expiration-time');
-        $p256dhKey = $request->request->get('p256dh');
-        $authToken = $request->request->get('auth');
+
+        $params = array();
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $params = json_decode($content, true); // 2nd param to get as array
+            $endpoint = $params['endpoint'];
+            $expirationTime = $params['expiration'];
+            $p256dhKey = $params['p256dh'];
+            $authToken = $params['auth'];
+        } else {
+            $endpoint = $request->request->get('endpoint');
+            $expirationTime = $request->request->get('expiration');
+            $p256dhKey = $request->request->get('p256dh');
+            $authToken = $request->request->get('auth');
+        }
 
         $sub = Subscription::createInstance($endpoint, $expirationTime, $p256dhKey, $authToken);
         $manager = $this->get('doctrine.orm.default_entity_manager');
