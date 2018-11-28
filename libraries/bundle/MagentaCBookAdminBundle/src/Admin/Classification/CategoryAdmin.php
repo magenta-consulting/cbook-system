ae99500c590751e3350e475a79e1ab803e00cb37
binhle410 <binh@sunrise.vn>
2018-11-28 20:27:25 +0700

    getOrganisationFieldNameTrait

--- libraries/bundle/MagentaCBookAdminBundle/src/Admin/Classification/CategoryAdmin.php
+++ libraries/bundle/MagentaCBookAdminBundle/src/Admin/Classification/CategoryAdmin.php
@@ -9,6 +9,7 @@ use Magenta\Bundle\CBookAdminBundle\Admin\BaseAdminTrait;
 use Magenta\Bundle\CBookAdminBundle\Form\Type\OrgAwareCategorySelectorType;
 use Magenta\Bundle\CBookModelBundle\Entity\Classification\Category;
 use Magenta\Bundle\CBookModelBundle\Entity\Classification\Context;
+use Magenta\Bundle\CBookModelBundle\Entity\Organisation\IndividualGroup;
 use Magenta\Bundle\CBookModelBundle\Entity\Organisation\Organisation;
 use Magenta\Bundle\CBookModelBundle\Entity\User\User;
 use Magenta\Bundle\CBookModelBundle\Service\User\UserService;
@@ -31,82 +32,85 @@ use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Validator\Constraints\Valid;
 
-class CategoryAdmin extends SonataCategoryAdmin {
-	use BaseAdminTrait {
-		getOrganisationFieldName as protected getOrganisationFieldNameTrait;
-		configureRoutes as protected configureRoutesTrait;
+class CategoryAdmin extends SonataCategoryAdmin
+{
+    use BaseAdminTrait {
+        configureRoutes as protected configureRoutesTrait;
 //		configureFormFields as protected configureFormFieldsTrait;
-	}
-
+    }
+    
     /**
      * @var Category $subject
      */
-	protected $subject;
-	
-	protected function getOrganisationFieldName($class) {
-		return 'organisation';
-	}
-	
-	public function configureRoutes(RouteCollection $collection) {
-		parent::configureRoutes($collection);
-		$collection->add('createCategory');
-	}
-	
-	protected function configureTabMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null) {
-		parent::configureTabMenu($menu, $action, $childAdmin);
-	}
-	
-	protected function configureFormFields(FormMapper $formMapper) {
-		parent::configureFormFields($formMapper);
-		$formMapper->remove('parent');
-		$formMapper->remove('media');
-		$formMapper->remove('position');
-		/** @var Category $subject */
-		$subject  = $this->getSubject();
-		$position = $this->hasSubject() && null !== $this->getSubject()->getPosition() ? $this->getSubject()->getPosition() : 0;
-		$formMapper->with('Options', [])
-		           ->add('enabled', CheckboxType::class, [
-			           'required' => false,
-			           'help'     => 'If a book is not enabled, no one can view it except Admins.'
-		           ])
-		           ->add('public', CheckboxType::class, [
-			           'required' => false,
-			           'help'     => 'To set whether a book is public or private.'
-		           ])
-		           ->add('accessGrantedGroups', ModelType::class, [
-			           'btn_add'  => false,
-			           'required' => false,
-			           'property' => 'name',
-			           'multiple' => true,
-			           'help'     => 'Access Granted Groups enable the selected groups to view private books. This has no effects when a book is public.'
-		
-		           ])
-		           ->add('accessDeniedGroups', ModelType::class, [
-			           'btn_add'  => false,
-			           'required' => false,
-			           'property' => 'name',
-			           'multiple' => true,
-			           'help'     => 'Access Denied Groups prevent the selected groups from viewing public books. This has no effects when a book is private.'
-		           ])
-		           ->end();
-		
-		$formMapper
-			->with('General')
-			->add('parent', OrgAwareCategorySelectorType::class, [
-				'organisation'  => $this->getCurrentOrganisation(),
-				'category'      => $this->getSubject() ?: null,
-				'model_manager' => $this->getModelManager(),
-				'class'         => $this->getClass(),
-				'required'      => true,
-				'context'       => $this->getConfigurationPool()->getContainer()->get('doctrine')->getRepository(Context::class)->find(Context::DEFAULT_CONTEXT),
-				// $this->getSubject()->getContext(),
-				'btn_add'       => false
-			])
-			->end();
+    protected $subject;
+    
+    public function configureRoutes(RouteCollection $collection)
+    {
+        parent::configureRoutes($collection);
+        $collection->add('createCategory');
+    }
+    
+    protected function configureTabMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null)
+    {
+        parent::configureTabMenu($menu, $action, $childAdmin);
+    }
+    
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        parent::configureFormFields($formMapper);
+        $formMapper->remove('parent');
+        $formMapper->remove('media');
+        $formMapper->remove('position');
+        /** @var Category $subject */
+        $subject = $this->getSubject();
+        $position = $this->hasSubject() && null !== $this->getSubject()->getPosition() ? $this->getSubject()->getPosition() : 0;
+        
+        $groupQuery = $this->getFilterByOrganisationQueryForModel(IndividualGroup::class);
+        $formMapper->with('Options', [])
+            ->add('enabled', CheckboxType::class, [
+                'required' => false,
+                'help' => 'If a book is not enabled, no one can view it except Admins.'
+            ])
+            ->add('public', CheckboxType::class, [
+                'required' => false,
+                'help' => 'To set whether a book is public or private.'
+            ])
+            ->add('accessGrantedGroups', ModelType::class, [
+                'query' => $groupQuery,
+                'btn_add' => false,
+                'required' => false,
+                'property' => 'name',
+                'multiple' => true,
+                'help' => 'Access Granted Groups enable the selected groups to view private books. This has no effects when a book is public.'
+            
+            ])
+            ->add('accessDeniedGroups', ModelType::class, [
+                'query' => $groupQuery,
+                'btn_add' => false,
+                'required' => false,
+                'property' => 'name',
+                'multiple' => true,
+                'help' => 'Access Denied Groups prevent the selected groups from viewing public books. This has no effects when a book is private.'
+            ])
+            ->end();
+        
+        $formMapper
+            ->with('General')
+            ->add('parent', OrgAwareCategorySelectorType::class, [
+                'organisation' => $this->getCurrentOrganisation(),
+                'category' => $this->getSubject() ?: null,
+                'model_manager' => $this->getModelManager(),
+                'class' => $this->getClass(),
+                'required' => true,
+                'context' => $this->getConfigurationPool()->getContainer()->get('doctrine')->getRepository(Context::class)->find(Context::DEFAULT_CONTEXT),
+                // $this->getSubject()->getContext(),
+                'btn_add' => false
+            ])
+            ->end();
 
 //		$keys = $formMapper->keys();
 //		$key  = array_pop($keys);
 //		array_unshift($keys, $key);
 //		$formMapper->reorder($keys);
-	}
+    }
 }
