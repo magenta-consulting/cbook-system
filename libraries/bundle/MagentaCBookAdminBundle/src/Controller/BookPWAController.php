2fb828684463863fcec93b13c9be0ea0393d3869
binhle410 <binh@sunrise.vn>
2018-12-16 23:50:47 +0700

    hell

--- libraries/bundle/MagentaCBookAdminBundle/src/Controller/BookPWAController.php
+++ libraries/bundle/MagentaCBookAdminBundle/src/Controller/BookPWAController.php
@@ -2,30 +2,24 @@
 
 namespace Magenta\Bundle\CBookAdminBundle\Controller;
 
-use Magenta\Bundle\CBookModelBundle\Entity\Messaging\Message;
 use Magenta\Bundle\CBookModelBundle\Entity\Organisation\Organisation;
 use Magenta\Bundle\CBookModelBundle\Entity\System\ProgressiveWebApp\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
-
 
 class BookPWAController extends Controller
 {
-    
     public function savePushSubscriptionAction($orgSlug, $accessCode, $employeeCode, Request $request)
     {
         $this->get('magenta_book.individual_service')->checkAccess($accessCode, $employeeCode, $orgSlug);
         $member = $this->get('magenta_book.individual_service')->getMemberByPinCodeEmployeeCode($accessCode, $employeeCode);
-        
+
         /**
-         * {"endpoint":"https://fcm.googleapis.com/fcm/send/e1vbWsIR4ew:APA91bEyuhRU3gVr93sd-A5Kyls6RD4OhY4wxwfK-hPisTP_zSr21X33XiLHNcPOYPZDJE2tswNvksaCFDBALIdeyWp5WncOID5-QAcqUiHhoh25Xyi6phuvAjke1uJxc7Ys1S9fTsYc","expirationTime":null,"keys":{"p256dh":"BIT1e4n6h29d0NLBjLOrtBDJ69mOXGke3aq9rjRH3OG9cuihV2mvB_8_ATcaYt6pKcVjv8TR5kwfCzSNA1WeAOo","auth":"OOhi3n0xQ9LI-a5k7AsCDg"}}
+         * {"endpoint":"https://fcm.googleapis.com/fcm/send/e1vbWsIR4ew:APA91bEyuhRU3gVr93sd-A5Kyls6RD4OhY4wxwfK-hPisTP_zSr21X33XiLHNcPOYPZDJE2tswNvksaCFDBALIdeyWp5WncOID5-QAcqUiHhoh25Xyi6phuvAjke1uJxc7Ys1S9fTsYc","expirationTime":null,"keys":{"p256dh":"BIT1e4n6h29d0NLBjLOrtBDJ69mOXGke3aq9rjRH3OG9cuihV2mvB_8_ATcaYt6pKcVjv8TR5kwfCzSNA1WeAOo","auth":"OOhi3n0xQ9LI-a5k7AsCDg"}}.
          */
-        
-        $params = array();
+        $params = [];
         $content = $request->getContent();
         if (!empty($content)) {
             $params = json_decode($content, true); // 2nd param to get as array
@@ -41,19 +35,23 @@ class BookPWAController extends Controller
             $authToken = $request->request->get('auth');
             $contentEncoding = $params['content-encoding'];
         }
-        
+
+        if (empty(endpoint) || empty($expirationTime) || empty($p256dhKey) || empty($authToken) || empty($contentEncoding)) {
+            return new JsonResponse('EMPTY INPUT');
+        }
         $sub = Subscription::createInstance($endpoint, $expirationTime, $p256dhKey, $authToken, $contentEncoding);
         $manager = $this->get('doctrine.orm.default_entity_manager');
         $sub->setIndividualMember($member);
         $manager->persist($sub);
         $manager->flush($sub);
+        
         return new JsonResponse('OK');
     }
-    
+
     public function manifestAction($orgSlug, $accessCode, $employeeCode, Request $request)
     {
         $orgRepo = $this->getDoctrine()->getRepository(Organisation::class);
-        
+
         $org = $orgRepo->findOneBy(['slug' => $orgSlug]);
         if (empty($org)) {
             throw new NotFoundHttpException();
@@ -65,28 +63,29 @@ class BookPWAController extends Controller
             'employeeCode' => $employeeCode,
         ]);
         $response->headers->set('Content-Type', 'application/json');
+
         return $response;
     }
-    
+
     public function serviceWorkerAction($orgSlug, Request $request)
     {
         $orgRepo = $this->getDoctrine()->getRepository(Organisation::class);
-        
+
         $org = $orgRepo->findOneBy(['slug' => $orgSlug]);
-        
+
         if (empty($org)) {
             throw new NotFoundHttpException();
         }
         $employeeCode = $request->get('employeeCode');
         $accessCode = $request->get('accessCode');
-        
+
         $response = $this->render('@MagentaCBookAdmin/App/ProgressiveWebApp/service-worker-app.js.twig', [
             'orgSlug' => $orgSlug,
             'accessCode' => $accessCode,
-            'employeeCode' => $employeeCode
-        
+            'employeeCode' => $employeeCode,
         ]);
         $response->headers->set('Content-Type', 'application/javascript');
+
         return $response;
     }
 }
